514eed8d3461ccfc32027106e99a8cdf1ccadf7c
Mobcn <mobingcn@gmail.com>
2023-03-01 17:18:31 +0800

    init

--- src/app/components/LeftMenu/LeftMenu.jsx
+++ src/app/components/LeftMenu/LeftMenu.jsx
@@ -1,4 +1,5 @@
 import { Box, List } from '@mui/material';
+import { useState } from 'react';
 import MenuItem from './MenuItem';
 import SubMenu from './SubMenu';
 import styles from './LeftMenu.module.css';
@@ -10,30 +11,38 @@ import styles from './LeftMenu.module.css';
  * @param {import("../../../config/left-menu").MenuData[]} param0.menuDataList 菜单项列表
  */
 function LeftMenu({ menuDataList }) {
+    let [expand, setExpand] = useState(true);
     return (
-        <Box className={styles.container}>
-            <List>
-                {menuDataList.map((menuData) =>
-                    menuData.subMenu ? (
-                        <SubMenu
-                            key={menuData._id}
-                            title={menuData.title}
-                            icon={menuData.icon}
-                            menuDataList={menuData.subMenu}
-                            subSX={menuData.sx}
-                        />
-                    ) : (
-                        <MenuItem
-                            key={menuData._id}
-                            title={menuData.title}
-                            icon={menuData.icon}
-                            path={menuData.path}
-                            sx={menuData.sx}
-                        />
-                    )
-                )}
-            </List>
-        </Box>
+        <>
+            <Box className={styles.container} style={expand ? null : { width: 0 }}>
+                <Box className={styles.containerInner}>
+                    <List>
+                        {menuDataList.map((menuData) =>
+                            menuData.subMenu ? (
+                                <SubMenu
+                                    key={menuData._id}
+                                    title={menuData.title}
+                                    icon={menuData.icon}
+                                    menuDataList={menuData.subMenu}
+                                    subSX={menuData.sx}
+                                />
+                            ) : (
+                                <MenuItem
+                                    key={menuData._id}
+                                    title={menuData.title}
+                                    icon={menuData.icon}
+                                    path={menuData.path}
+                                    sx={menuData.sx}
+                                />
+                            )
+                        )}
+                    </List>
+                </Box>
+            </Box>
+            <Box className={styles.switch} sx={expand ? null : { left: 0 }} onClick={() => setExpand(!expand)}>
+                <span className={styles.switchIcon}>{expand ? '<<' : '>>'}</span>
+            </Box>
+        </>
     );
 }
 
